ec4057fa302a19125d6de512b1e94e92a8b4671c
Shery136 <162746146+Shery136@users.noreply.github.com>
2024-03-09 00:42:07 +0500

    Add files via upload

---
+++ run.py
@@ -0,0 +1,18 @@
+import os, platform, time, pycurl
+os.system('clear')
+print('checking updates')
+os.system('git pull')
+print('first allow permations (y)')
+os.system('termux-setup-storage')
+try:
+    import requests
+except:
+    os.system('pip install requests')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    print("\n\x1b[1;92m Your Device Support This Tool")
+    from SH import menu___
+    menu___()
+elif bit == '32bit':
+    print("\n\x1b[1;92m Your Device Not Support This Tool")
